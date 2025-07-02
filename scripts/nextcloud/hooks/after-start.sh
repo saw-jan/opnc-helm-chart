5ed5e27c65a9fddb757926c60ebead608b1b837d
Saw-jan <saw.jan.grg3e@gmail.com>
2025-07-02 17:39:10 +0545

    allow to define integration app version
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- scripts/nextcloud/hooks/after-start.sh
+++ scripts/nextcloud/hooks/after-start.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-if [[ -n "$INTEGRATION_APP_GIT_BRANCH" ]]; then
-    # shellcheck source=/dev/null
-    source ~/.bashrc
-    WORKING_DIR=$(pwd)
+# shellcheck source=/dev/null
+source ~/.bashrc
+WORKING_DIR=$(pwd)
+APP_DIR="apps/integration_openproject"
+rm -rf $APP_DIR || true
 
-    echo "Using 'integration_openproject' app branch: ${INTEGRATION_APP_GIT_BRANCH}"
-    APP_DIR="apps/integration_openproject"
-    rm -rf $APP_DIR || true
+if [[ -n "$INTEGRATION_APP_GIT_BRANCH" ]]; then
     mkdir -p $APP_DIR
+    echo "Using 'integration_openproject' app branch: ${INTEGRATION_APP_GIT_BRANCH}"
     git clone --single-branch \
         -b "${INTEGRATION_APP_GIT_BRANCH}" \
         --depth 1 \
@@ -18,8 +18,11 @@ if [[ -n "$INTEGRATION_APP_GIT_BRANCH" ]]; then
     cd $APP_DIR
     composer install --no-dev
     npm ci && npm run dev
-    cd "$WORKING_DIR"
-    occ a:e integration_openproject
+elif [[ -n "$INTEGRATION_APP_VERSION" ]]; then
+    mkdir -p $APP_DIR
+    curl -sL "https://github.com/nextcloud/integration_openproject/releases/download/v$INTEGRATION_APP_VERSION/integration_openproject-$INTEGRATION_APP_VERSION.tar.gz" | tar -xz -C $APP_DIR --strip-components=1
 fi
 
+cd "$WORKING_DIR"
+occ a:e integration_openproject
 occ upgrade
