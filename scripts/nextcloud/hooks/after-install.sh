c86b7f8fa477323e38b460baf148d882bbfb5ae8
Saw-jan <saw.jan.grg3e@gmail.com>
2025-07-03 15:38:36 +0545

    configurable values
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- scripts/nextcloud/hooks/after-install.sh
+++ scripts/nextcloud/hooks/after-install.sh
@@ -7,7 +7,7 @@ occ security:certificates:import /etc/ssl/certs/ca-certificates.crt
 occ config:app:set --value=1 user_oidc store_login_token
 occ config:system:set user_oidc --type boolean --value="true" oidc_provider_bearer_validation
 occ user_oidc:provider Keycloak \
-    -c 'nextcloud' \
-    -s 'RGF6WCCOzm2bmnqgRelaIA5fp3XaMTIH' \
-    -d 'https://keycloak.local/realms/opnc/.well-known/openid-configuration' \
-    -o 'openid profile email api_v3'
+    -c "nextcloud" \
+    -s "RGF6WCCOzm2bmnqgRelaIA5fp3XaMTIH" \
+    -d "https://$KEYCLOAK_HOST/realms/opnc/.well-known/openid-configuration" \
+    -o "openid profile email api_v3"
