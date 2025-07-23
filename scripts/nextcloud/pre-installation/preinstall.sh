182a913467bac121696e4f28cb15abde949c6cfc
Saw-jan <saw.jan.grg3e@gmail.com>
2025-07-23 10:42:21 +0545

    fix: server nextcloud from branch
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- scripts/nextcloud/pre-installation/preinstall.sh
+++ scripts/nextcloud/pre-installation/preinstall.sh
@@ -2,20 +2,24 @@
 
 set -eo pipefail
 
+WORKING_DIR=$(pwd)
+
 function wait_for_command() {
     local cmd="$1"
-    local retries=12
+    local max_wait=60
+    local wait_time=0
     local delay=5
 
-    for ((i = 0; i < retries; i++)); do
+    while [[ $wait_time -lt $max_wait ]]; do
         if command -v "$cmd" >/dev/null; then
             return 0
         fi
         echo "Waiting for '$cmd' to be available..."
         sleep "$delay"
+        wait_time=$((wait_time + delay))
     done
 
-    echo "Command '$cmd' not found after $((retries * delay)) seconds."
+    echo "Command '$cmd' not found after $max_wait seconds."
     exit 1
 }
 
@@ -23,3 +27,26 @@ wait_for_command git
 wait_for_command node
 wait_for_command npm
 wait_for_command composer
+
+if [ -n "$NC_SERVE_GIT_BRANCH" ]; then
+    SRC_DIR=/tmp/server
+    # sync server files to the web root
+    rsync -rlD --delete --chmod=755 --chown=www-data:www-data $SRC_DIR/ /var/www/html
+    # npm ci
+    # npm run build
+
+    # max_wait=300 # 5 minutes
+    # wait_time=0
+    # delay=5
+    # synced_file="/usr/src/nextcloud/synced"
+
+    # while [ ! -f "$synced_file" ] && [ $wait_time -lt $max_wait ]; do
+    #     echo "Waiting for Nextcloud source files to be ready..."
+    #     sleep "$delay"
+    #     wait_time=$((wait_time + delay))
+    # done
+    # if [ ! -f "$synced_file" ]; then
+    #     echo "Nextcloud source files not ready after $max_wait seconds."
+    #     exit 1
+    # fi
+fi
--- scripts/nextcloud/presetup.sh
+++ scripts/nextcloud/presetup.sh
@@ -22,17 +22,22 @@ if [ -n "$NC_SERVE_GIT_BRANCH" ]; then
     WEB_ROOT="/var/www/html"
     echo "Pulling Nextcloud server from github branch '${NC_SERVE_GIT_BRANCH}'..."
 
-    SRC_DIR=/usr/src/nextcloud
+    SRC_DIR=/tmp/server
     rm -rf $SRC_DIR || true
     mkdir -p $SRC_DIR
     # get nextcloud server
     git clone --single-branch -b "${NC_SERVE_GIT_BRANCH}" --depth 1 https://github.com/nextcloud/server.git $SRC_DIR
+    cp -f $SRC_DIR/version.php /usr/src/nextcloud/version.php
     git config -f $SRC_DIR/.gitmodules submodule.3rdparty.shallow true
-    (cd $SRC_DIR && git submodule update --init)
+    cd "$SRC_DIR"
+    git submodule update --init
     mkdir -p $SRC_DIR/custom_apps
     mkdir -p $SRC_DIR/data
-    # sync server files to the web root
-    rsync -a --delete --chmod=755 --chown=www-data:www-data $SRC_DIR/ $WEB_ROOT
-    # fix permissions
-    chown www-data: -R $WEB_ROOT/data $WEB_ROOT/custom_apps $WEB_ROOT/.htaccess
+    # npm ci
+    # npm run build
+    # # sync server files to the web root
+    # rsync -a --delete --chmod=755 --chown=www-data:www-data $SRC_DIR/ $WEB_ROOT
+    # # fix permissions
+    # chown www-data: -R $WEB_ROOT/data $WEB_ROOT/custom_apps $WEB_ROOT/.htaccess
+    # touch $SRC_DIR/synced
 fi
