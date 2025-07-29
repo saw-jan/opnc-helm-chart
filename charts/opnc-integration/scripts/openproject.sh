539c4f91bc282bd2fba606b04dc609413cef43e5
Saw-jan <saw.jan.grg3e@gmail.com>
2025-07-29 14:23:57 +0545

    fix: openproject setup script
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- charts/opnc-integration/scripts/openproject.sh
+++ charts/opnc-integration/scripts/openproject.sh
@@ -3,19 +3,29 @@
 set -eo pipefail
 
 if [[ -n "$OP_GIT_SOURCE_BRANCH" ]]; then
-    APP_PATH=/appgit
-    mkdir $APP_PATH && cd $APP_PATH
+    APP_PATH=$HOME/appgit
+    mkdir "$APP_PATH" && cd "$APP_PATH"
     export APP_PATH
 
+    echo "Cloning OpenProject from branch: $OP_GIT_SOURCE_BRANCH"
     git clone --branch "$OP_GIT_SOURCE_BRANCH" --depth 1 --single-branch "https://github.com/opf/openproject" "$APP_PATH"
 
-    "$APP_PATH"/docker/prod/setup/bundle-install.sh
-    "$APP_PATH"/docker/prod/setup/precompile-assets.sh
-    "$APP_PATH"/docker/prod/setup/postinstall-common.sh
+    bash "$APP_PATH"/docker/prod/setup/bundle-install.sh
+    bash "$APP_PATH"/docker/prod/setup/precompile-assets.sh
+    bash "$APP_PATH"/docker/prod/setup/postinstall-common.sh
 
     rm -f "$APP_PATH"/config/database.yml
     cp "$APP_PATH"/config/database.production.yml "$APP_PATH"/config/database.yml
+    rm -f "$HOME/.irbrc"
     ln -s "$APP_PATH"/docker/prod/setup/.irbrc /home/"$APP_USER"/
 fi
 
-./docker/prod/entrypoint.sh ./docker/prod/web
+args=()
+for arg in "$@"; do
+    arg="${arg//\/app\//./}"
+    args+=("$arg")
+done
+
+cd "$APP_PATH"
+
+./docker/prod/entrypoint.sh "${args[@]}"
